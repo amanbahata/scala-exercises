e4c09d75de1c0303a9098638eb5c48fd1be429fc
Aman Bahata <bahataaman@gmail.com>
2017-10-02 23:49:27 +0100

    Ads exercises with List

--- Greeting.scala
+++ Greeting.scala
@@ -5,8 +5,31 @@
 val greeting = new Array[String](3)
 greeting(0) = "Hello"
 greeting(1) = ", "
-greeting(2) = "World!!"
+greeting(2) = "World!!\n"
 
 for( greet <- greeting) {
 	print(greet)
 }
+//===================================================
+
+val numNames = Array("zero","one","two")
+
+val numNames2 = Array.apply("three","four","five")
+
+println(numNames(1))
+println(numNames2(1))
+
+
+/*
+* Exercise using lists
+**/
+
+val oneTwoThree = List(1,2,3)
+
+//List concatination
+val oneTwo = List(1,2)
+val threeFour = List(3,4)
+val oneTwoThreeFour = oneTwo ::: threeFour
+
+println(oneTwo + " and " + threeFour + " were not mutated.")
+println("Thus,  " + oneTwoThreeFour + " is a new list.")
